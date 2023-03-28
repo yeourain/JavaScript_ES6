33afeec9322fd54218f4511fd8d54bba0bed7aed
TAEHYUN LEE <yeourain@gmail.com>
2023-03-28 21:08:14 +0900

    JS: variable

---
+++ 1.variable.js
@@ -0,0 +1,26 @@
+// 변수 Variables
+//     :값을 저장하는 공간
+//     :자료를 저장할 수 있는 이름이 주어진 기억장소
+
+// 변수 선언 Variable declaration
+//     let a; -> let(키워드) a(변수이름);(문의 끝)
+// 변수 선언과 값의 할당 variable declaration and assignment
+//     let a = 0; -> let(키워드) a(변수이름) =(할당연산자) 0(값);(문의 끝)
+// 값의 재할당 value reassigning
+//     a = 1; -> a(변수이름) =(할당연산자) 1(값);(문의 끝)
+
+let a = 0;
+console.log(a);
+
+a = 1;
+console.log(a);
+
+let b;
+console.log(b);
+
+b = 2;
+console.log(b);
+
+// 변수 이름짓기 Naming Variables
+//     :저장된 값을 잘 나타낼 수 있는 의미 있는 이름
+//     :"구체적일 수록 좋다"
