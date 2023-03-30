fc387c9cc485aaa4ea82b34b594301eca93a8c23
TAEHYUN LEE <yeourain@gmail.com>
2023-03-30 17:50:33 +0900

    JS: empty

--- 6.empty.js
+++ 6.empty.js
@@ -8,7 +8,7 @@ console.log(variable);
 let activeItem; // 아직 활성화 된 아이템이 있는지 없는지 모르는 상태
 activeItem = null; // 활성화된 아이템이 없는 상태
 
-console.log(typeof 123);
-console.log(typeof '123');
-console.log(typeof null);
-console.log(typeof undefined);
+console.log(typeof 123); // number
+console.log(typeof '123'); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
